2629f4ba2cf8aced0b8cca0f8c3efdbc156e0dc0
Jonathan Obenland <jobenland1@gmail.com>
2019-08-21 11:10:28 -0400

    Fixed Redunancy

--- gui.py
+++ gui.py
@@ -265,7 +265,7 @@ class Ui_MainWindow(object):
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.textBrowser.setPlaceholderText(_translate("MainWindow", "There has been an error getting help and about. ERROR: 100"))
-        self.groupBox_5.setTitle(_translate("MainWindow", "Information and Assistance"))
+        self.groupBox_5.setTitle(_translate("MainWindow", "Information"))
         self.textBrowser_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
